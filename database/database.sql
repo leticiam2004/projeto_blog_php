085efdc8080e66b8b8d7f1ecd38eb368ce491b2f
Alejandro Castellanos <112025060+Tallikarcz@users.noreply.github.com>
2025-02-09 23:15:24 -0300

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -30,7 +30,10 @@ CREATE TABLE IF NOT EXISTS `contato` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Data exporting was unselected.
+-- Dumping data for table database.contato: ~3 rows (approximately)
+INSERT INTO `contato` (`id`, `nome_completo`, `email`, `assunto`, `situacao`, `problema`) VALUES
+	(1, 'alejandro', 'alejandro_castellano@hotmail.com', 'resolvido', 'Novo', ''),
+	(2, 'alejandro', 'alejandro_castellano@hotmail.com', 'adsafcsargavsdfgcrs ayudaaaaaaaaa', 'Resolvido', '');
 
 -- Dumping structure for table database.produtos
 CREATE TABLE IF NOT EXISTS `produtos` (
@@ -41,7 +44,26 @@ CREATE TABLE IF NOT EXISTS `produtos` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Data exporting was unselected.
+-- Dumping data for table database.produtos: ~18 rows (approximately)
+INSERT INTO `produtos` (`id`, `nome`, `preco`, `image`) VALUES
+	(1, 'Pokéball', 200, 'https://iili.io/2b9Lvqu.png'),
+	(2, 'GreatBall', 600, 'https://iili.io/2b9LSgj.png'),
+	(3, 'UltraBall', 1200, 'https://iili.io/2b9L80b.png'),
+	(4, 'Cura Veneno', 100, 'https://iili.io/2b9L6mB.png'),
+	(5, 'Cura Paralisia', 200, 'https://iili.io/2b9L4eV.png'),
+	(6, 'Cura Sono', 200, 'https://iili.io/2b9LizP.png'),
+	(7, 'Cura Gelo', 200, 'https://iili.io/2b9Lr5Q.png'),
+	(8, 'Cura Queimadura', 200, 'https://iili.io/2b9Lese.png'),
+	(9, 'Cura Total', 600, 'https://iili.io/2b9Lgdx.png'),
+	(10, 'Poção', 300, 'https://iili.io/2b9LsX1.png'),
+	(11, 'Super Poção', 700, 'https://iili.io/2b9Lt1a.png'),
+	(12, 'Hiper Poção', 1200, 'https://iili.io/2b9LLLF.png'),
+	(13, 'Max Poção', 2500, 'https://iili.io/2b9LZqg.png'),
+	(14, 'Repelente', 350, 'https://iili.io/2b9LDrJ.png'),
+	(15, 'Super Repelente', 500, 'https://iili.io/2b9Lp7R.png'),
+	(16, 'Max Repelente', 700, 'https://iili.io/2b9Lmdv.png'),
+	(17, 'Reviver', 1500, 'https://iili.io/2b9Lyep.png'),
+	(41, 'alejandro', 55765, 'https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png');
 
 -- Dumping structure for table database.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -53,7 +75,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Data exporting was unselected.
+-- Dumping data for table database.users: ~2 rows (approximately)
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `admin`) VALUES
+	(10, 'Darkian', 'alejandro_castellano@hotmail.com', '$2y$10$VYQ9FD33MjfGrZkZ6NKHM.3A1bLwqOOjFa5q8Go/QHcU7wi.e6wby', 1);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
