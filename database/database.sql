800b4ebc88f677bf23406f34da745ba88ff020c7
Letícia Magnan <100086541+leticiam2004@users.noreply.github.com>
2025-02-17 16:34:40 -0300

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               11.3.2-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             12.8.0.6908
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.0.30 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -15,38 +15,38 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dumping database structure for database
-CREATE DATABASE IF NOT EXISTS `database` /*!40100 DEFAULT CHARACTER SET armscii8 COLLATE armscii8_bin */;
+-- Copiando estrutura do banco de dados para database
+CREATE DATABASE IF NOT EXISTS `database` /*!40100 DEFAULT CHARACTER SET armscii8 COLLATE armscii8_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `database`;
 
--- Dumping structure for table database.contato
+-- Copiando estrutura para tabela database.contato
 CREATE TABLE IF NOT EXISTS `contato` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome_completo` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `assunto` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   `situacao` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT 'Novo',
   `problema` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Dumping data for table database.contato: ~3 rows (approximately)
-INSERT INTO `contato` (`id`, `nome_completo`, `email`, `assunto`, `situacao`, `problema`) VALUES
+-- Copiando dados para a tabela database.contato: ~3 rows (aproximadamente)
+INSERT IGNORE INTO `contato` (`id`, `nome_completo`, `email`, `assunto`, `situacao`, `problema`) VALUES
 	(6, 'Ash Ketchum', 'ash@pallet.com', 'Questões sobre entrega', 'Novo', 'Estou aguardando a entrega de 10 Pokébolas e 5 Poções para uma missão urgente em Johto. Já passaram 5 dias e ainda não recebi nenhuma atualização sobre o status do pedido. Poderiam verificar, por favor?'),
 	(7, 'Misty Waterflower', 'misty@cerulean.com', 'Sugestões de novos produtos', 'Novo', 'Seria incrível se vocês pudessem adicionar itens específicos para treinadores de Pokémon de água, como Poções especiais para recuperação rápida após batalhas subaquáticas. Isso ajudaria muito em treinamentos intensos!'),
 	(8, 'Brock Harrison', 'brock@pewtergym.com', 'Suporte técnico', 'Novo', 'Comprei uma Pokébola personalizada, mas ela parece estar com defeito, pois não está funcionando corretamente ao tentar capturar Pokémon. Vocês poderiam me orientar sobre o que fazer ou trocar o item?');
 
--- Dumping structure for table database.produtos
+-- Copiando estrutura para tabela database.produtos
 CREATE TABLE IF NOT EXISTS `produtos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
-  `preco` float NOT NULL DEFAULT 0,
+  `preco` float NOT NULL DEFAULT '0',
   `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Dumping data for table database.produtos: ~17 rows (approximately)
-INSERT INTO `produtos` (`id`, `nome`, `preco`, `image`) VALUES
+-- Copiando dados para a tabela database.produtos: ~17 rows (aproximadamente)
+INSERT IGNORE INTO `produtos` (`id`, `nome`, `preco`, `image`) VALUES
 	(1, 'Pokéball', 200, 'https://iili.io/2b9Lvqu.png'),
 	(2, 'GreatBall', 600, 'https://iili.io/2b9LSgj.png'),
 	(3, 'UltraBall', 1200, 'https://iili.io/2b9L80b.png'),
@@ -65,18 +65,18 @@ INSERT INTO `produtos` (`id`, `nome`, `preco`, `image`) VALUES
 	(16, 'Max Repelente', 700, 'https://iili.io/2b9Lmdv.png'),
 	(17, 'Reviver', 1500, 'https://iili.io/2b9Lyep.png');
 
--- Dumping structure for table database.users
+-- Copiando estrutura para tabela database.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL DEFAULT '0',
-  `email` varchar(50) NOT NULL DEFAULT '0',
-  `password` varchar(250) NOT NULL DEFAULT '0',
-  `admin` tinyint(4) NOT NULL DEFAULT 0,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE armscii8_bin NOT NULL DEFAULT '0',
+  `email` varchar(50) COLLATE armscii8_bin NOT NULL DEFAULT '0',
+  `password` varchar(250) COLLATE armscii8_bin NOT NULL DEFAULT '0',
+  `admin` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
 
--- Dumping data for table database.users: ~4 rows (approximately)
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `admin`) VALUES
+-- Copiando dados para a tabela database.users: ~4 rows (aproximadamente)
+INSERT IGNORE INTO `users` (`id`, `username`, `email`, `password`, `admin`) VALUES
 	(13, 'admin', 'admin@admin.com', '$2y$10$Yeiq48lwUcVjAu/.yaDnxOI27TF.hnxV/P..S.M37rUgiPNY7MI4e', 1),
 	(14, 'ash', 'ash@pallet.com', '$2y$10$Gt3BbNfSiKc4.h7ePYwg2ukw1p.gLN.XY90UJ3Q79G7kspFYz3AlS', 0),
 	(15, 'misty', 'misty@cerulean.com', '$2y$10$hCHjY4k3KmoI1w.oUl9khuaNo1/r4QpFKcI9McHKqQuFgetvhVlDe', 0),
